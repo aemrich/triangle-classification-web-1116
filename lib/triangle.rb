c32696b4a0e8abfa29734eae0f877185ac68aa31
aemrich <adam.emrich@gmail.com>
2016-12-05 19:21:16 -0500

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+#require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :first, :second, :third
+
+  def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
+  end
+
+
+  def kind
+
+    if first <= 0 || second <= 0 || third <= 0
+      raise TriangleError
+    elsif first + second <= third || second + third <= first || first + third <= second
+      raise TriangleError
+    end
+
+    if first == second && second == third && third == first
+      :equilateral
+    elsif first != second && second == third || first == third && second != third || first == second && second != third
+      :isosceles
+    elsif first != second && second != third && third != first
+      :scalene
+    end
+  end
+
+
+end
+
+class TriangleError < StandardError
 end
